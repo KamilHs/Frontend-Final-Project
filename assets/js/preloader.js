29d41aa9c95bf242bf449c141c3b00abcba937b9
KamilHs <kamilsalimli1940@gmail.com>
2019-08-30 19:44:58 +0400

    Comments were added to all html Elements. Also forgotten Footer Section in feature-single.html page was added in it

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,3 +1,6 @@
+$(document).on('scroll', function () {
+    $(".preloader-wrapper").css('top', `${$(document).scrollTop()}px`);
+})
 $(window).on("load", function () {
     $(".preloader-wrapper").css('top', `${$(document).scrollTop()}px`)
     $(".preloader-wrapper").fadeOut("slow");
